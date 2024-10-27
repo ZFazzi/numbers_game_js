2107f22c9108a4074b1529911d46b8bb40fbdca5
Zakia Fazzi <kia.fazzi@gmail.com>
2024-10-27 22:09:44 +0100

    small changes

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+/*EXERCISE 1: Guess the random number 
+
+Create a function guessNumberGame that generates a random number between 1 and 10. 
+
+The function should repeatedly prompt the user to guess the number until the correct number is guessed. 
+
+Use a while loop to keep asking the user for input until they guess correctly.  
+
+Provide feedback if the guess is too high or too low. */
+
+
+
 function guessNumberGame() {
     // Gives a random number between 1 and 10
     const targetNumber = Math.floor(Math.random() * 10) + 1;
